2f4304ceedf7652335e450a19de14714f4706408
CelestialCrafter <celestialcraftermc@gmail.com>
2024-02-15 01:05:15 -0600

    handle EPIPE

--- app.js
+++ app.js
@@ -32,40 +32,48 @@ const router = new AlphaRouter({
 const transformStream = new Transform({
 	transform(chunk, encoding, callback) {
 		try {
-			const data = JSON.parse(chunk);
+			const data = String(chunk).split(':');
+			const transaction = JSON.parse(data.slice(0, -1).join(':'));
+			const keyId = data[data.length - 1];
 	
 			const tokens = {
-				base: new Token(chainId, data.tokens.base.address, data.tokens.base.decimals),
-				mod: new Token(chainId, data.tokens.mod.address, data.tokens.mod.decimals)
+				base: new Token(chainId, transaction.pair[0].address, transaction.pair[0].decimals),
+				mod: new Token(chainId, transaction.pair[1].address, transaction.pair[1].decimals)
 			}
 	
 			const amount = CurrencyAmount.fromRawAmount(
 				tokens.base,
-				data.amount
+				transaction.amount
 			);
 	
 			router.route(
 				amount,
 				tokens.mod,
-				data.action === 'sell' ? TradeType.EXACT_OUTPUT : TradeType.EXACT_INPUT,
+				transaction.action === 'sell' ? TradeType.EXACT_OUTPUT : TradeType.EXACT_INPUT,
 				{
-					recipient: data.address,
+					recipient: transaction.address,
 					slippageTolerance: new Percent(20, 10000), // 0.2%
 					deadline: Math.floor(Date.now() / 1000) + 60 * 10,
 					type: SwapType.SWAP_ROUTER_02
 				}
-			).then(({ methodParameters: { calldata, value } }) => callback(null, `${calldata}:${value}`));
+			).then(route => {
+				if (!route) throw new Error("no route");
+				const { methodParameters: { calldata, value } } = route;
+				callback(null, `${calldata}:${value}:${keyId}`);
+			});
 		} catch (error) {
 			console.error(error);
 			callback(null, null);
 		}
 	}
-})
-
+});
 
 pipeline(
 	process.stdin,
 	transformStream,
 	process.stdout,
 	err => err && console.error(err)
-)
+);
+
+process.stdout.on('error', err => err.code === 'EPIPE' ? process.exit(0) : null);
+
