87340041092ddd696eeca40f317edf1ae688fbb9
CelestialCrafter <celestialcraftermc@gmail.com>
2024-02-17 13:49:31 -0600

    change to http add logging

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-import { Transform, pipeline } from 'node:stream';
+import express from 'express';
+import bunyanMiddleware from 'bunyan-middleware';
+import epipebomb from 'epipebomb';
 import { AlphaRouter, CurrencyAmount, SwapType } from '@uniswap/smart-order-router';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { Percent, Token, TradeType } from '@uniswap/sdk-core';
+import { createLogger } from 'bunyan';
+
+epipebomb();
+
+const log = createLogger({
+	name: 'transaction-router',
+	streams: [
+		{
+			level: 'trace',
+			stream: process.stdout
+		},
+		{
+			level: 'trace',
+			path: 'route.log'
+		}
+	]
+});
 
 const provider = new JsonRpcProvider(process.argv[2]);
 const chainId = Number(process.argv[3]);
@@ -11,69 +30,78 @@ const router = new AlphaRouter({
 	provider
 });
 
+log.info({ chainId, providerUrl: process.argv[2] }, 'initialized router');
+
 /*
-	Chunk Format:
+	Transaction Format:
 	{
 		amount: Number
 		action: String (buy | sell)
-		tokens: {
-			base: {
+		pair: [
+			{
 				address: String,
 				decimals: Number
 			},
-			mod: {
+			{
 				address: String,
 				decimals: Number
 			}
-		}
+		]
 	}
 */
 
-const transformStream = new Transform({
-	transform(chunk, encoding, callback) {
-		try {
-			const data = String(chunk).split(':');
-			const transaction = JSON.parse(data.slice(0, -1).join(':'));
-			const keyId = data[data.length - 1];
-	
-			const tokens = {
-				base: new Token(chainId, transaction.pair[0].address, transaction.pair[0].decimals),
-				mod: new Token(chainId, transaction.pair[1].address, transaction.pair[1].decimals)
-			}
-	
-			const amount = CurrencyAmount.fromRawAmount(
-				tokens.base,
-				transaction.amount
-			);
-	
-			router.route(
-				amount,
-				tokens.mod,
-				transaction.action === 'sell' ? TradeType.EXACT_OUTPUT : TradeType.EXACT_INPUT,
-				{
-					recipient: transaction.address,
-					slippageTolerance: new Percent(20, 10000), // 0.2%
-					deadline: Math.floor(Date.now() / 1000) + 60 * 10,
-					type: SwapType.SWAP_ROUTER_02
-				}
-			).then(route => {
-				if (!route) throw new Error("no route");
-				const { methodParameters: { calldata, value } } = route;
-				callback(null, `${calldata}:${value}:${keyId}`);
-			});
-		} catch (error) {
-			console.error(error);
-			callback(null, null);
+const routeTransaction = async transaction => {
+	const tokens = {
+		base: new Token(chainId, transaction.pair[0].address, transaction.pair[0].decimals),
+		mod: new Token(chainId, transaction.pair[1].address, transaction.pair[1].decimals)
+	};
+
+	const amount = CurrencyAmount.fromRawAmount(tokens.base, transaction.amount);
+
+	const route = await router.route(
+		amount,
+		tokens.mod,
+		transaction.action === 'sell' ? TradeType.EXACT_OUTPUT : TradeType.EXACT_INPUT,
+		{
+			recipient: transaction.address,
+			slippageTolerance: new Percent(20, 10000), // 0.2%
+			deadline: Math.floor(Date.now() / 1000) + 60 * 10,
+			type: SwapType.SWAP_ROUTER_02
 		}
+	);
+	if (!route) throw new Error('No Route');
+	const {
+		methodParameters: { calldata, value }
+	} = route;
+	return `${calldata}:${value}`;
+};
+
+const app = express();
+app.use(express.json());
+app.use(
+	bunyanMiddleware({
+		logger: log,
+		headerName: 'X-Request-Id',
+		propertyName: 'reqId',
+		logName: 'reqId'
+	})
+);
+
+app.get('/ping', (req, res) => res.status(200).send('pong'));
+
+app.post('/route', async (req, res) => {
+	if (!req.body) return res.status(400).send('No Body');
+
+	try {
+		req.log.info({ transaction: req.body }, 'routing transaction');
+		const route = await routeTransaction(req.body);
+		req.log.info({ route }, 'routed transaction');
+		res.status(200).send(route);
+	} catch (err) {
+		req.log.error({ error: err }, 'error routing');
+		res.status(500).send(err.toString());
 	}
 });
 
-pipeline(
-	process.stdin,
-	transformStream,
-	process.stdout,
-	err => err && console.error(err)
-);
-
-process.stdout.on('error', err => err.code === 'EPIPE' ? process.exit(0) : null);
-
+const port = 6278;
+app.listen(port, () => log.info({ port }, 'server listening'));
